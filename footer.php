d97c5a07ab356383b0f39c5395c9d9fdb67b7045
Alluysios <arribaallu@gmail.com>
2019-02-19 16:58:49 -0700

    Fixed header content / Building Footer pt1
    mobile view for header fixed.

--- footer.php
+++ footer.php
@@ -15,6 +15,11 @@
 
 	<footer id="colophon" class="footer site-footer">
 		<div class="footer_info site-info">
+			<h3 class="heading-tertiary">Social Media.</h3>
+			<div class="footer__sm">
+				<i class="fab fa-twitter"></i>
+				<i class="fab fa-instagram"></i>
+			</div>
 			<a href="<?php echo esc_url( __( 'http://lunar.web.dmitcapstone.ca/lunarperformance/', 'lunar_performance' ) ); ?>" class="footer__link">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
@@ -22,6 +27,12 @@
 				?>
 			</a>
 		</div><!-- .site-info -->
+
+		<div class="footer__contact">
+			<h3 class="heading-tertiary">Contact</h3>
+			<p class="footer__phone-info">Phone: 587-999-9999</p>
+			<p class="footer__email-info">Email: lunarperformance@gmail.com</p>
+		</div><!-- .contact -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
