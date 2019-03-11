4e7a1cc8f43b119d6b10db9b2cd44d94e10878a1
Alluysios <30941996+Alluysios@users.noreply.github.com>
2019-03-11 09:31:19 -0600

    .

--- functions.php
+++ functions.php
@@ -73,8 +73,8 @@ if ( ! function_exists( 'lunar_performance_setup' ) ) :
 		 * @link https://codex.wordpress.org/Theme_Logo
 		 */
 		add_theme_support( 'custom-logo', array(
-			'height'      => 250,
-			'width'       => 250,
+			'height'      => 200,
+			'width'       => 200,
 			'flex-width'  => true,
 			'flex-height' => true,
 		) );
