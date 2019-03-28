61a5a284b1e582c7bc474cc62413f520f2f4f42e
Alluysios <arribaallu@gmail.com>
2019-03-27 22:34:54 -0600

    update program

--- archive-program.php
+++ archive-program.php
@@ -6,7 +6,7 @@
 <?php while ( have_posts() ) : the_post(); ?>
 <?php
 if(get_field('program_name')) { $program_name = get_field('program_name'); }
-if(get_field('program_description')) { $program_description = get_field('program_description'); } 
+if(get_field('program_description')) { $program_description = substr(get_field('program_description'), 0, 160).". . . . . ."; } 
 if(get_field('program_img')) { $program_img = get_field('program_img'); } 
 ?>
 
@@ -15,18 +15,14 @@ if(get_field('program_img')) { $program_img = get_field('program_img'); }
         <figure class="wp-block-image">
         <img src="<?php echo $program_img['url']; ?>" alt="" class="wp-image-216"></figure>
         <div class="tprograms__iteminfo">
-            <h3 class="tprograms__iteminfo-heading" id="break"><?php echo $program_name; ?></h3>
+            <h3 class="tprograms__iteminfo-heading"><?php echo $program_name; ?></h3>
             <p class="tprograms__iteminfo-description"><?php echo $program_description; ?></p>
             <a href="<?php echo get_permalink(get_the_ID()); ?>" class="tprograms__btn">Learn More → 
             </a>
         </div>
     </div>
 <?php endwhile; // End of the loop. ?>
-    
-
-
 </div>
-
 
 
 
